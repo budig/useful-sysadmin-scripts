67cc9ea61b64ea4acb60adbce178c20a5de358cb
Endika Gil <endika.gil@gmail.com>
2022-03-30 09:47:15 +0200

    Added more options. Still in progress

--- netdisco/netdisco.py
+++ netdisco/netdisco.py
@@ -1,7 +1,21 @@
+#### STILL IN PROGRESS ####
+'''
+    Option 1 - Buscar equipo: 
+        * Have to parse json variable to show data
+    Option 2 - Equipos conectados a switch:
+        * Filtering with active status = 1 it is not running on pandas
+    Option 3 - Ver puertos VLAN en switch:
+        * Must check the pandas result. I see more registers that must be
+    API_KEY:
+        * We retreive the api key using .env file that is not uploaded on git. Anyway it is not the best option to store a password in plain text.
+          We should implement the use of the operating system keyring.
+'''
+
 import requests
 import os
 import json
 import pandas as pd
+import socket
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -19,17 +33,19 @@ def request_api_key():
 
 menu_options = {
     1: 'Buscar equipo',
-    2: 'Option 2',
-    3: 'Option 3',
+    2: 'Equipos conectados a switch',
+    3: 'Ver puertos VLAN en switch',
     4: 'Exit',
 }
+
+def resolve_name_to_ip(device):
+    return socket.gethostbyname(device)
 
 def print_menu():
     for key in menu_options.keys():
         print (key, '--', menu_options[key] )
 
 def menu_1_search_host(hostname):
-    #print('\'Opción 1\' - Buscar equipo: '+hostname)
     api_key=request_api_key()
     r = requests.get(NETDISCO_URL+'/api/v1/search/node',
                     headers={'Accept': 'application/json',
@@ -43,12 +59,30 @@ def menu_1_search_host(hostname):
     # ..
     # .. (USE PANDAS?)
      
+def menu_2_get_nodes_on_device(device):
+    api_key=request_api_key()
+    r = requests.get(NETDISCO_URL + '/api/v1/object/device/' + resolve_name_to_ip(device) + '/nodes',
+                    headers={'Accept': 'application/json',
+                            'Authorization': api_key})
+                            
+    # pretty print the JSON stucture
+    #print(json.dumps(r.json(), indent=2, sort_keys=True))
+    data_search = r.json()
+    df = pd.DataFrame(data_search)
+    activos = df.loc[df['active']==1]
+    print(activos)
 
-def option2():
-     print('Handle option \'Option 2\'')
-
-def option3():
-     print('Handle option \'Option 3\'')
+def menu_3_get_port_vlans_switch(device):
+    api_key=request_api_key()
+    r = requests.get(NETDISCO_URL + '/api/v1/object/device/' + resolve_name_to_ip(device) + '/port_vlans',
+                    headers={'Accept': 'application/json',
+                            'Authorization': api_key})
+                            
+    # pretty print the JSON stucture
+    #print(json.dumps(r.json(), indent=2, sort_keys=True))
+    data_search = r.json()
+    df = pd.DataFrame(data_search)
+    print(df)
 
 if __name__=='__main__':
     while(True):
@@ -69,10 +103,20 @@ if __name__=='__main__':
             menu_1_search_host(hostname)
 
         elif option == 2:
-            option2()
+            try:
+                device = str(input("Introduce el nombre del switch ['XXXXXX.sufijoDNS']: "))
+            except ValueError:
+                print("Formato incorrecto")
+                continue
+            menu_2_get_nodes_on_device(device)
 
         elif option == 3:
-            option3()
+            try:
+                device = str(input("Introduce el nombre del switch ['XXXXXX.sufijoDNS']: "))
+            except ValueError:
+                print("Formato incorrecto")
+                continue
+            menu_3_get_port_vlans_switch(device)
 
         elif option == 4:
             print('Adiós')
